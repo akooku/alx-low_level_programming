e8cd41e4a60b1864e98bcd935f4ecd710c41cf58
Ako Oku <akooku12@gmail.com>
2023-02-21 11:37:44 -0800

    Update alphabet

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,13 +1,14 @@
 #include "main.h"
 
 /**
- * main - Entry point
+ * print_alphabet -  prints lowercase alphabet letters
  *
  * Does not return a value (void)
  */
 void print_alphabet(void)
 {
 	char c;
+
 	for (c = 'a'; c <= 'z'; c++)
 		_putchar(c);
 	_putchar('\n');
