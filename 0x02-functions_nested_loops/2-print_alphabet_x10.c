7c5542c77ea0e5d668e4ac813bef57e88a874a28
Ako Oku <akooku12@gmail.com>
2023-02-21 12:11:34 -0800

    Add alphabet x10

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - prints 10 times the alphabet, in lowercase
+ *
+ * Return: Void, does not return a value
+ */
+void print_alphabet_x10(void)
+{
+	char c;
+	int n = 0;
+
+	while (n < 10)
+	{
+		for (c = 'a'; c <= 'z'; c++)
+			_putchar(c);
+		n++;
+		_putchar('\n');
+	}
+}
