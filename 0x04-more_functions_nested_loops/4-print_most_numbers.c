0d00528623362d821572c9466d876aa8758e48e5
Ako Oku <akooku12@gmail.com>
2023-02-23 14:43:12 -0800

    Fix error

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -12,17 +12,13 @@ void print_most_numbers(void)
 {
 	int i = 0;
 
-	while (i < 10)
+	for (; i < 10; i++)
 	{
 		if (i == 2 || i == 4)
 		{
 			continue;
 		}
-		else
-		{
-			_putchar(i + '0');
-		}
-		i++;
+		_putchar(i + '0');
 	}
 	_putchar('\n');
 }
