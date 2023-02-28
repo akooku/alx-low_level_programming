b829794f3c146ea9c2dce91ee276af88f5b11901
Ako Oku <akooku12@gmail.com>
2023-02-27 20:13:35 -0800

    Adds 4-print_rev.c

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+ * print_rev - prints a string in reverse
+ *
+ * @s: string
+ *
+ * return: void
+ */
+
+void print_rev(char *s)
+{
+	int longi = 0;
+	int r;
+
+	while (*s != '\0')
+	{
+		longi++;
+		s++
+	}
+
+	s--;
+
+	for (r = longi; r > 0; r--)
+	{
+		_putchar(*s);
+		s--;
+	}
+
+	_putchar('\n');
+}
