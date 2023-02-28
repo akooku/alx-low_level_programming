07a4546751e98b53c6fb03bccf20d5cedfb660e0
Ako Oku <akooku12@gmail.com>
2023-02-27 17:35:48 -0800

    Adds 1-swap.c

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * swap_int - swaps the values of two integers
+ *
+ * @a: integer 1
+ * @b: integer 2
+ *
+ * Return: Void
+ */
+
+void swap_int(int *a, int *b)
+{
+	*a = *b;
+	*b = *a;
+}
