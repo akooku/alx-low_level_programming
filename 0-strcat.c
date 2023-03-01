85d1db1ae6b99f3882ad10b6bba6222859586eb6
Ako Oku <akooku12@gmail.com>
2023-03-01 07:31:25 -0800

    Adds 0-strcat.c

---
+++ 0-strcat.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * *_strcat - concatenates two strings
+ *
+ * @dest: destination string
+ * @src: source string
+ *
+ * Return: a pointer to the resulting string dest
+ */
+
+char *_strcat(char *dest, char *src)
+{
+	while (*dest)
+	{
+		dest++;
+	}
+
+	while (*src)
+	{
+		*dest = *src;
+		dest++;
+		src++;
+	}
+
+	*dest = '\0';
+
+	return (dest);
+}
--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-This is a readme file
+Learning about ponters, arrays, and strings... again
