7502e63112c4a04bbc0feb4fb3d222037d6f0918
Ako Oku <akooku12@gmail.com>
2023-03-02 17:49:22 -0800

    Adds 7-leet.c

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,31 @@
+#include "main.h"
+#include <stdio.h>
+/**
+  * leet - encodes a string into 1337O (leetcode)
+  * @ch: char array
+  * Return: char type
+  */
+char *leet(char *ch)
+{
+	int i = 0, j;
+	char leet[9] = {'O', 'L', '?', 'E', 'A', '?', '?', 'T', '\0'};
+
+	while (ch[i] != '\0')
+	{
+		j = 0;
+
+		while (leet[j] != '\0')
+		{
+			if (ch[i] == leet[j] + 32 || ch[i] == leet[j])
+			{
+				ch[i] = j + '0';
+			}
+
+			j++;
+		}
+
+		i++;
+	}
+
+	return (ch);
+}
