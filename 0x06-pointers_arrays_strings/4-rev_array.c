996c02434221e7b62f493e942984543037ede5eb
Ako Oku <akooku12@gmail.com>
2023-03-02 17:28:04 -0800

    Adds 4-rev_array.c

---
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * reverse_array - reverses the content of an array of integers
+ *
+ * @a: array
+ * @n: integers
+ *
+ * Return: void
+ */
+
+void reverse_array(int *a, int n)
+{
+	int i = 0, j = n - 1, temp;
+
+	while (i < j)
+	{
+		temp = a[i];
+		a[i] = a[j];
+		a[j] = temp;
+		i++;
+		j--;
+	}
+}
