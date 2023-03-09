542d5c2189281c3ead8a8601b5e6ae8da2fd1265
Ako Oku <akooku12@gmail.com>
2023-03-09 09:36:54 -0800

    Fixes 100-is_palindrome.c

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -1,41 +1,31 @@
 #include "main.h"
-
 /**
- * check_palidrome - checks if palidrome
- *
+ * check_palindrome - checks if a string is a palindrome recursively
  * @s: string
- * @start: start of string
- * @end: end of string
- *
- * Return: 1 if palidrome, 0 otherwise
+ * @start: starting index
+ * @end: ending index
+ * Return: 1 if palindrome, 0 if not palindrome
  */
-
-int check_palidrome(char *s, int start, int end)
+int check_palindrome(char *s, int start, int end)
 {
 	if (start >= end)
 	{
 		return (1);
 	}
-
 	if (s[start] != s[end])
 	{
 		return (0);
 	}
-
-	return (check_palidrome(s, start + 1, end - 1));
+	return (check_palindrome(s, start + 1, end - 1));
 }
-
 /**
- * is_palidrome - checks if palidrome
- *
+ * is_palindrome - checks if a string is a palindrome
  * @s: string
- *
- * Return: 1 if palindrome, 0 otherwise
+ * Return: 1 if palindrome, 0 if not palindrome
  */
-
-int is_palidrome(char *s)
+int is_palindrome(char *s)
 {
 	int len = strlen(s);
 
-	return (check_palidrome(s, 0, len - 1));
+	return (check_palindrome(s, 0, len - 1));
 }
