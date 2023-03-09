38f6d1c95527915ccd42e8824a50a4d04094557d
Ako Oku <akooku12@gmail.com>
2023-03-09 10:19:08 -0800

    Fixes 100-is_palindrome.c

--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -8,5 +8,4 @@ int _sqrt_recursion(int n);
 int is_prime_number(int n);
 int is_prime_number_helper(int n, int i);
 int is_palindrome(char *s);
-int check_palindrome(char *s, int start, int end)
 int wildcmp(char *s1, char *s2);
