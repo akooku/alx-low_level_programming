1b7fd37033987f6c9b31db31a79341c123c0563a
Ako Oku <akooku12@gmail.com>
2023-03-09 10:23:30 -0800

    Fixes 100-is_palindrome.c

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -1,29 +1,31 @@
 #include "main.h"
-#include <string.h>
-
 /**
- * is_palindrome - checks if palindrome
- *
+ * check_palindrome - checks if a string is a palindrome recursively
  * @s: string
- *
- * Return: 1 if palindrome, 0 otherwise
+ * @start: starting index
+ * @end: ending index
+ * Return: 1 if palindrome, 0 if not palindrome
  */
-
+int check_palindrome(char *s, int start, int end)
+{
+	if (start >= end)
+	{
+		return (1);
+	}
+	if (s[start] != s[end])
+	{
+		return (0);
+	}
+	return (check_palindrome(s, start + 1, end - 1));
+}
+/**
+ * is_palindrome - checks if a string is a palindrome
+ * @s: string
+ * Return: 1 if palindrome, 0 if not palindrome
+ */
 int is_palindrome(char *s)
 {
 	int len = strlen(s);
 
-	if (len <= 1)
-	{
-		return (1);
-	}
-
-	if (s[0] == s[len - 1])
-	{
-		return (is_palindrome(s + 1, len - 2));
-	}
-	else
-	{
-		return (0);
-	}
+	return (check_palindrome(s, 0, len - 1));
 }
--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -7,5 +7,6 @@ int _pow_recursion(int x, int y);
 int _sqrt_recursion(int n);
 int is_prime_number(int n);
 int is_prime_number_helper(int n, int i);
+int check_palindrome(char *s, int start, int end);
 int is_palindrome(char *s);
 int wildcmp(char *s1, char *s2);
