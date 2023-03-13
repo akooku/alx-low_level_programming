05901db01f2b1ec4ac7efcea4389404ff122fb4e
Ako Oku <akooku12@gmail.com>
2023-03-13 15:46:04 -0700

    Fixes 0-create_array.c

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -14,19 +14,19 @@
 
 char *create_array(unsigned int size, char c)
 {
+	char *arr = (char *)malloc(sizeof(char) * size);
+	unsigned int i;
+
 	if (size == 0)
 	{
 		return (NULL);
 	}
-
-	char *arr = (char *)malloc(sizeof(char) * size);
 
 	if (arr == NULL)
 	{
 		return (NULL);
 	}
 
-	unsigned int i;
 	for (i = 0; i < size; i++)
 	{
 		arr[i] = c;
