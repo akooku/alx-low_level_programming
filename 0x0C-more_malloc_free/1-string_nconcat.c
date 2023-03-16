af5b6d4425e836ec3e6df26453d4050dca1d6d8e
Ako Oku <akooku12@gmail.com>
2023-03-16 11:38:10 -0700

    Adds 1-string_nconcat.c

---
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -0,0 +1,50 @@
+#include "main.h"
+#include <stdlib.h>
+/**
+ * *string_nconcat - concatenates two strings
+ *
+ * @s1: string 1
+ * @s2: string 2
+ * @n: number of bytes
+ *
+ * Return: pointer on success, NULL on failure
+ */
+char *string_nconcat(char *s1, char *s2, unsigned int n)
+{
+	int i, j;
+	int len1 = 0, len2 = 0;
+	char *newstr;
+
+	if (s1 == NULL)
+		_putchar(' ');
+	if (s2 == NULL)
+		_putchar(' ');
+
+	while (s1[len1])
+	{
+		len1++;
+	}
+	while (s2[len2])
+	{
+		len2++;
+	}
+	if (n >= len2)
+		n = len2;
+
+	newstr = malloc(sizeof(char) * (len1 + n + 1));
+	if (newstr == NULL)
+		return (NULL);
+
+	for (i = 0; i < len1; i++)
+	{
+		newstr[i] = s1[i];
+	}
+
+	for (j = 0; j < n; j++)
+	{
+		newstr[i + j] = s2[j];
+	}
+
+	newstr[i + j] = '\0';
+	return (newstr);
+}
