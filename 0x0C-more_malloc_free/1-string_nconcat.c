06336e0ecbad70c720cc0f5e5c37f711daddaf3a
Ako Oku <akooku12@gmail.com>
2023-03-16 12:13:31 -0700

    1-string_nconcat.c

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -34,7 +34,7 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 			n = len2;
 	}
 
-	newstr = malloc(sizeof(char) * (len1 + len2 + 1));
+	newstr = malloc(sizeof(*newstr) * (len1 + len2 + 1));
 	if (newstr == NULL)
 	{
 		free(newstr);
