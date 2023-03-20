4a93aabc36cf2f46342a3914f0ab0a7c08ff4c80
Ako Oku <akooku12@gmail.com>
2023-03-20 14:10:41 -0700

    Fixes error 1

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,6 +1,18 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - defines a dog
+ * @name: the name of the dog
+ * @age: the dogs age
+ * @owner: the dogs owner
+ */
+
 struct dog
 {
 	char *name;
 	float age;
 	char *owner;
 }
+
+#endif
