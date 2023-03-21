1b8af112618f9c34e87f4c07ae405b034456a80a
Ako Oku <akooku12@gmail.com>
2023-03-20 21:57:11 -0700

    Fixes error (Attempt 3)

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -1,7 +1,49 @@
 #include "dog.h"
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
+
+/**
+ * _strlen - returns the length of a string
+ *
+ * @s: string to measure
+ *
+ * Return: length of the string
+ */
+
+int _strlen(char *s)
+{
+	int i = 0;
+
+	while (s[i] != '\0')
+	{
+		i++;
+	}
+
+	return (i);
+}
+
+/**
+ * _strcpy - copies a string from src to dest
+ *
+ * @dest: destination string
+ * @src: source string
+ *
+ * Return: pointer to dest
+ */
+
+char *_strcpy(char *dest, char *src)
+{
+	int i;
+
+	for (i = 0; src[i] != '\0'; i++)
+	{
+		dest[i] = src[i];
+	}
+
+	dest[i] = '\0';
+
+	return (dest);
+}
 
 /**
  * *new_dog - creates a new dog
@@ -14,27 +56,31 @@
 
 dog_t *new_dog(char *name, float age, char *owner)
 {
-	dog_t *new_dog = (dog_t *) malloc(sizeof(dog_t));
+	dog_t *new_dog = malloc(sizeof(dog_t));
 
 	if (new_dog == NULL)
 	{
 		return (NULL);
 	}
 
-	new_dog->name = strdup(name);
+	new_dog->name = malloc(_strlen(name) + 1);
 	if (new_dog->name == NULL)
 	{
 		free(new_dog);
 		return (NULL);
 	}
+	_strcpy(new_dog->name, name);
+
 	new_dog->age = age;
-	new_dog->owner = strdup(owner);
+
+	new_dog->owner = malloc(_strlen(name) + 1);
 	if (new_dog->owner == NULL)
 	{
 		free(new_dog->name);
 		free(new_dog);
 		return (NULL);
 	}
+	_strcpy(new_dog->owner, owner);
 
 	return (new_dog);
 }
