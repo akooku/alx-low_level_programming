85a880d12a61205f2e4ac9e926af7d8ec78ad589
Ako Oku <akooku12@gmail.com>
2023-04-03 18:10:20 -0700

    Adds 2-add_nodeint.c

--- 0x13-more_singly_linked_lists/2-add_nodeint.c
+++ 0x13-more_singly_linked_lists/2-add_nodeint.c
@@ -13,12 +13,12 @@ listint_t *add_nodeint(listint_t **head, const int n)
 {
 	listint_t *first_node = malloc(sizeof(listint_t));
 
+	first_node->n = n;
 	first_node->next = *head;
 
 	if (first_node == NULL)
 		return (NULL);
 
-	first_node->n = n;
 	*head = first_node;
 
 	return (first_node);
